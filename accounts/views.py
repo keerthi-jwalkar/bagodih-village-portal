4333eb8fe4025c5ef24919d639cb24396baee68a
keerthi jwalkar <keerthijwalkar@gmail.com>
2026-07-18 23:33:38 +0530

    uprade project changes

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,36 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+
 from django.contrib.auth import authenticate, login, logout
 
 
 def login_page(request):
+
     if request.method == "POST":
+
         username = request.POST.get("username")
         password = request.POST.get("password")
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
 
         if user is not None:
-            login(request, user)
-            return redirect("home")
-        else:
-            messages.error(request, "Invalid username or password.")
 
-    return render(request, 'accounts/login.html')
+            login(request, user)
+
+            return redirect("/")
+
+        else:
+
+            messages.error(request, "Invalid Username or Password")
+
+            return redirect("login")
+
+    return render(request, "accounts/login.html")
 
 
 def register_page(request):
@@ -35,7 +48,23 @@ def register_page(request):
         if password != confirm_password:
             messages.error(request, "Passwords do not match.")
             return redirect("register")
-
+        # Password Length
+        if len(password) < 6:
+         messages.error(request, "Password must be at least 6 characters.")
+         return redirect("register")
+        #uppercase
+        if not any(char.isupper() for char in password):
+         messages.error(request, "Password must contain at least one uppercase letter.")
+         return redirect("register")
+        #lowercase
+        if not any(char.islower() for char in password):
+         messages.error(request, "Password must contain at least one lowercase letter.")
+         return redirect("register")
+        #number
+        if not any(char.isdigit() for char in password):
+         messages.error(request, "Password must contain at least one number.")
+         return redirect("register")
+        
         # Username Exists
         if User.objects.filter(username=username).exists():
             messages.error(request, "Username already exists.")
@@ -47,6 +76,8 @@ def register_page(request):
             return redirect("register")
 
         # Create User
+        print("Creating user...")
+        
         User.objects.create_user(
             username=username,
             first_name=first_name,
@@ -54,11 +85,19 @@ def register_page(request):
             email=email,
             password=password
         )
+        print("User created successfully")
 
         messages.success(request, "Account created successfully. Please login.")
         return redirect("login")
 
     return render(request, "accounts/register.html")
+
+
+
 def logout_page(request):
+
     logout(request)
+
+    messages.success(request, "Logged out successfully.")
+
     return redirect("login")
